39483c2801fd33c50ba841266669fdfcbf5444ed
wilberquito <wbq.software@gmail.com>
2024-04-11 20:27:39 +0200

    feat: custom data sample transformer

---
+++ HousePricing/src/transforms.py
@@ -0,0 +1,17 @@
+import torch
+
+class ToTensor(object):
+    """Transform sample into Tensor
+    """
+
+    def __call__(self, sample):
+        if "target" in sample:
+            inputs, target = sample["inputs"], sample["target"]
+            inputs = torch.tensor(inputs, dtype=torch.float32)
+            target = torch.tensor(target, dtype=torch.float32)
+            return {"inputs": inputs, "target": target}
+        else:
+            inputs = sample["inputs"]
+            inputs = torch.tensor(inputs, dtype=torch.float32)
+            return {"inputs": inputs}
+
