a4644ba51d7e43180b313e34efd1fc05088409cb
wilberquito <wbq.software@gmail.com>
2024-04-18 09:58:14 +0200

    generating fake run names for wandb experiment

--- HousePricing/src/util.py
+++ HousePricing/src/util.py
@@ -1,4 +1,14 @@
 import os
+from faker import Faker
 
 def optim_workers():
     return max(1, os.cpu_count() - 1)
+
+def fake_name():
+    fake = Faker()
+    fake_name = fake.name().split()[0]
+    fake_pwd = fake.password(
+        length=8, special_chars=False,
+        digits=True, upper_case=True, lower_case=False
+    )
+    return fake_name + "_" + fake_pwd
