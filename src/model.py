eeadea48485e5047428170fcad267acb84479f74
wilberquito <wbq.software@gmail.com>
2024-04-28 01:12:47 +0200

    test with new model

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import lightning as L
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from src.loss import RMSELoss
 
 
 class NeuralNetwork(L.LightningModule):
@@ -13,7 +13,7 @@ class NeuralNetwork(L.LightningModule):
         self.input_size = input_size
         self.lr = lr
 
-        print(f"[INFO]: Input size: {self.input_size}")
+        self.loss_fn = F.mse_loss
 
         self.net = nn.Sequential(
             nn.Linear(in_features=self.input_size, out_features=512),
@@ -31,12 +31,12 @@ class NeuralNetwork(L.LightningModule):
         output = self.net(inputs)
         return output
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch):
         inputs, target = batch["inputs"], batch["target"]
         if target.dim() == 1:
             target = target.view(target.size(0), -1)
         output = self(inputs)
-        loss = F.mse_loss(output, target)
+        loss = self.loss_fn(output, target)
         self.log("train_loss", loss)
         return loss
 
@@ -55,17 +55,14 @@ class NeuralNetwork(L.LightningModule):
         if target.dim() == 1:
             target = target.view(target.size(0), -1)
         output = self(inputs)
-        loss = F.mse_loss(output, target)
+        loss = self.loss_fn(output, target)
         return loss
 
     def predict_step(self, batch):
         ids = batch["id"]
         inputs = batch["inputs"]
         output = self(inputs).flatten()
-        return {
-            "id": ids,
-            "prediction": output
-        }
+        return {"id": ids, "prediction": output}
 
     def configure_optimizers(self):
         return torch.optim.AdamW(self.parameters(), lr=self.lr)
@@ -74,3 +71,33 @@ class NeuralNetwork(L.LightningModule):
         logger = self.trainer.logger
         self.save_model_dir = join(logger.save_dir, logger.name, logger.version)
         print(f"[INFO]: Logger save model dir: {self.save_model_dir}")
+
+
+class HousePricingNN(NeuralNetwork):
+    def __init__(self, input_size: int, lr=0.001):
+        super().__init__(input_size, lr)
+
+        # self.loss_fn = RMSELoss()
+        self.loss_fn = F.mse_loss
+
+        self.net = nn.Sequential(
+            nn.Linear(in_features=self.input_size, out_features=512),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=512, out_features=128),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=128, out_features=128),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=128, out_features=64),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=64, out_features=32),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=32, out_features=1),
+        )
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self.lr)
